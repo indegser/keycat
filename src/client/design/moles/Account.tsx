b28124f21935cd566102d8e95ba87099dd5031b3
indegser <indegser@gmail.com>
2019-02-26 15:25:27 +0900

    feat(Account): add bg color using identifier

--- src/client/design/moles/Account.tsx
+++ src/client/design/moles/Account.tsx
@@ -36,6 +36,8 @@ const Identicon = styled('div')`
   text-align: center;
   background: black;
   color: white;
+  font-size: 14px;
+  font-family: menlo, consolas, monospace;
 `;
 
 const Name = styled('div')`
@@ -50,6 +52,21 @@ const Address = styled('div')`
   word-break: break-all;
 `;
 
+const getHashCode = (str) => {
+  var hash = 0;
+  if (str.length == 0) return hash;
+  for (var i = 0; i < str.length; i++) {
+      hash = str.charCodeAt(i) + ((hash << 5) - hash);
+      hash = hash & hash; // Convert to 32bit integer
+  }
+  return hash;
+}
+
+const intToHSL = (int) => {
+  var shortened = int % 360;
+  return "hsl(" + shortened + ",100%,30%)";
+}
+
 const Account = ({ identifier }) => {
   const initial = identifier.slice(0, 1);
 
@@ -59,9 +76,17 @@ const Account = ({ identifier }) => {
     });
   }
 
+  const bg = intToHSL(getHashCode(identifier));
+
   return (
     <Container onClick={handleClick}>
-      <Identicon>{initial}</Identicon>
+      <Identicon
+        style={{
+          backgroundColor: bg,
+        }}
+      >
+        {initial}
+      </Identicon>
       <div>
         <Name>
           {identifier}
