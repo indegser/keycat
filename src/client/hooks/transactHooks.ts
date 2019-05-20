1de778896d1a2d869678ed833784227f3f1a6733
indegser <indegser@gmail.com>
2019-05-20 12:09:18 +0900

    use hook in transact

---
+++ src/client/hooks/transactHooks.ts
@@ -0,0 +1,22 @@
+import { useDispatch, useStore } from 'store/store';
+import { appActions } from 'store/ducks/appDuck';
+import * as eos from 'api/eos'
+import { sendMessage } from 'api/message';
+import { useCallback } from 'react';
+
+export const useTransact = () => {
+  const dispatch = useDispatch()
+  const { config: { client } } = useStore()
+
+  const transact = useCallback(async (values) => {
+    dispatch(appActions.setWorking({ working: true }))
+    const result = await eos.transact(values)
+    dispatch(appActions.setWorking({ working: false }))
+
+    sendMessage('transact', result, client)
+  }, [])
+
+  return {
+    transact,
+  }
+}
--- src/client/pages/transact/Transact.tsx
+++ src/client/pages/transact/Transact.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { Form, Formik } from 'formik';
-import { transact } from 'api/eos';
-import Identifier from 'design/moles/fields/Identifier';
 import TxPayload from './TxPayload';
 import Submit from 'design/moles/fields/Submit';
 import { getSearchParams } from 'utils/utils';
 import AccountField from 'design/moles/fields/AccountField';
 import PasswordField from 'design/moles/fields/PasswordField';
 import CardLayout from 'design/layouts/CardLayout';
-import { useStore } from 'store/store';
-import { sendMessage } from 'api/message';
+import { useTransact } from 'hooks/transactHooks';
 
 interface Props {
   path: string,
@@ -19,7 +16,7 @@ const Transact: React.SFC<Props> = (props) => {
   const params = getSearchParams()
   const account = params.get('account')
   const payload = decodeURIComponent(atob(params.get('p')))
-  const { config: { client } } = useStore()
+  const { transact } = useTransact()
 
   return (
     <CardLayout title="Sign Transaction">
@@ -29,10 +26,7 @@ const Transact: React.SFC<Props> = (props) => {
           payload,
           password: '',
         }}
-        onSubmit={async (values) => {
-          const result = await transact(values);
-          sendMessage('transact', result, client)
-        }}
+        onSubmit={transact}
       >
         {() => (
           <Form method="POST" noValidate>
