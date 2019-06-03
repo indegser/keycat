22e02e0be8b805935c640d72d0a4bac2479e6abd
indegser <indegser@gmail.com>
2019-06-03 15:52:18 +0900

    remove invalidation

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,3 @@ COMMIT_REF=$(echo $(git rev-parse HEAD) | cut -c1-7)
 yarn run build
 aws s3 sync public s3://keycat.co/${COMMIT_REF}
 aws cloudfront update-distribution --id E1QONAGSSYRR0Z --default-root-object ${COMMIT_REF}/index.html
-aws cloudfront create-invalidation --distribution-id E1QONAGSSYRR0Z --paths / /index.html
