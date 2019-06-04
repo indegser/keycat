dc6d4c62990ab6c07d61b473835b56f24506b254
indegser <indegser@gmail.com>
2019-06-04 14:15:35 +0900

    add Visit Playground button

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -38,8 +38,8 @@ const Headline = styled.h1`
 `
 
 const Secondary = styled.h2`
-  font-size: 24px;
-  color: #586069;
+  font-size: 22px;
+  color: #333;
   font-weight: 400;
   text-align: center;
   margin: 20px auto;
@@ -52,15 +52,50 @@ const Secondary = styled.h2`
 `
 
 const BrowserSupport = styled.div`
-  padding: 10px;
-  max-width: 320px;
-  margin: 0 auto;
   font-size: 14px;
-  color: var(--primary-color);
+  color: #808080;
+  margin: 0 auto;
+  margin-top: 20px;
+`
+
+const GotoArrow = styled.span`
+  margin-left: 4px;
+  display: inline-block;
+  transition: .2s transform ease;
+`
+
+const GotoPlayground = styled.div`
   text-align: center;
-  border: 1px solid;
-  border-radius: 4px;
-  line-height: 20px;
+  margin-top: 30px;
+
+  &:hover {
+    a {
+      border-color: #1074e7;
+      text-decoration: none;
+      color: #0366d6;
+    }
+
+    ${GotoArrow} {
+      transform: translateX(4px);
+    }
+  }
+
+  a {
+    padding: 20px 40px;
+    margin: 0 auto;
+    font-size: 16px;
+    color: #277bff;
+    text-align: center;
+    border: 1px solid;
+    border-color: rgba(16,116,231,.5);
+    user-select: none;
+    vertical-align: middle;
+    font-weight: 500;
+    display: inline-block;
+    cursor: pointer;
+    border-radius: 3px;
+  }
+
 `
 
 interface Props {
@@ -82,9 +117,17 @@ const About: React.SFC<Props> = () => {
           <Secondary className="balance-text">
             With Keycat, interacting with decentralized apps can be done inside your browser.
           </Secondary>
-          <BrowserSupport className="balance-text">
-            Keycat runs on Safari, Chrome, Firefox. Regardless of mobile or desktop.
-          </BrowserSupport>
+          <GotoPlayground>
+            <a href="/playground">
+              Visit Playground
+              <GotoArrow>
+                →
+              </GotoArrow>
+            </a>
+            <BrowserSupport className="balance-text">
+              Keycat runs on Safari, Chrome, Firefox regardless of mobile or desktop.
+            </BrowserSupport>
+          </GotoPlayground>
         </Container>
       )}
     />
