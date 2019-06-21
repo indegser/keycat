bf4b5d822a348fd698bc9f6a3d5f3bd15229c0ce
indegser <indegser@eosdaq.com>
2019-06-21 17:58:47 +0900

    add Route component

---
+++ src/design/moles/Route.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+interface RouteProps {
+  path?: string,
+  default?: boolean,
+}
+
+interface Props {
+  path?: string,
+  default?: boolean,
+  component: React.SFC<RouteProps>,
+}
+
+export const Route: React.SFC<Props> = ({ component: Comp, ...props }) => {
+  return <Comp {...props} />
+}
+
+export const AsyncRoute: React.SFC<Props> = ({ children }) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
--- src/pages/playground/Playground.tsx
+++ src/pages/playground/Playground.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { Router } from '@reach/router'
 import PageLayout from 'design/layouts/PageLayout';
 import PlaygroundHeader from './PlaygroundHeader';
 import Donate from './Donate';
+import { Route } from 'design/moles/Route';
 
 interface Props {
   path: string,
@@ -11,7 +13,11 @@ const Playground: React.SFC<Props> = () => {
   return (
     <PageLayout
       header={<PlaygroundHeader />}
-      main={<Donate />}
+      main={(
+        <Router path="playground">
+          <Route default component={Donate} />
+        </Router>
+      )}
     />
   )
 }
